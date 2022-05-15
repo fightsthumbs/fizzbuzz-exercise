ec2d6ba83e2068d681ff4a4027352b8dc5d7ce2f
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-05-15 11:05:19 -0600

    btn style, style tags

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ function fizzBuzz(x) {
   for (let index = 1; index < x; index++) {
     parStr = index;
     if (index % 3 == 0 && index % 5 == 0) {
-      parStr = 'FizzBuzz';
+      parStr = '<span class="red">FizzBuzz</span>';
     } else if (index % 3 == 0) {
-      parStr = 'Fizz';
+      parStr = '<span class="green">Fizz</span>';
     } else if (index % 5 == 0) {
-      parStr = 'Buzz';
+      parStr = '<span class="blue">Buzz</span>';
     } else {
       parStr = index;
     }
